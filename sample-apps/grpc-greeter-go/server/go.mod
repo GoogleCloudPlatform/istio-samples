3a9c6a642a6354a082e05d0095bdc7f570923d6b
Halvard Skogsrud <halvards@users.noreply.github.com>
2019-06-29 04:29:19 +1000

    Add gRPC greeter sample application (#27)
    * Add gRPC greeter sample application
    
    This sample application consists of a gRPC server and client.
    
    It is a adapted from the gRPC-Go helloworld example:
    https://github.com/grpc/grpc-go/tree/master/examples/helloworld
    
    The main changes from the original version are:
    
    - the server returns the hostname where it runs in a gRPC header; and
    
    - the client prints the hostname header from the gRPC response.
    
    This is useful for testing request routing and load balancing
    functionality.
    
    When running in a Kubernetes cluster, the hostname is the name of the
    Kubernetes Pod that served the request.
    
    * Moved and split K8s and Istio manifests
    
    - Moved K8s and Istio files to manifests/ directory
    
    - Split Istio manifest into one file per object
    
    - Created Istio Gateway manifests for both Ingress Gateway and ILB Gateway, so
      that the sample application can be used as either a public or interal
      service, respectively.

---
+++ sample-apps/grpc-greeter-go/server/go.mod
@@ -0,0 +1,25 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// https://github.com/golang/go/wiki/Modules
+
+module github.com/GoogleCloudPlatform/istio-samples/sample-apps/grpc-greeter-go/server
+
+go 1.12
+
+require (
+	github.com/golang/protobuf v1.3.1 // indirect
+	golang.org/x/net v0.0.0-20190620200207-3b0461eec859 // indirect
+	google.golang.org/grpc v1.21.1
+)
---
+++ sample-apps/grpc-greeter-go/server/server.go
@@ -0,0 +1,89 @@
+/*
+ *
+ * Original work Copyright 2015 gRPC authors
+ * Modified work Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/*
+ * Changes:
+ * 2019-06-24: SayHello returns hostname in response header
+ */
+
+//go:generate protoc -I ../helloworld --go_out=plugins=grpc:../helloworld ../helloworld/helloworld.proto
+
+// Package main implements a server for Greeter service.
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"net"
+	"os"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"
+	healthpb "google.golang.org/grpc/health/grpc_health_v1"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+)
+
+type greeterServer struct{}
+
+// SayHello implements helloworld.GreeterServer
+func (s *greeterServer) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.Name)
+// [START istio_samples_app_grpc_greeter_server_hostname]
+	hostname, err := os.Hostname()
+	if err != nil {
+		log.Printf("Unable to get hostname %v", err)
+	}
+	if hostname != "" {
+		grpc.SendHeader(ctx, metadata.Pairs("hostname", hostname))
+	}
+// [END istio_samples_app_grpc_greeter_server_hostname]
+	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
+}
+
+type healthServer struct{}
+
+// Check is used for health checks
+func (s *healthServer) Check(ctx context.Context, in *healthpb.HealthCheckRequest) (*healthpb.HealthCheckResponse, error) {
+	log.Printf("Handling Check request [%v]", in, ctx)
+	return &healthpb.HealthCheckResponse{Status: healthpb.HealthCheckResponse_SERVING}, nil
+}
+
+// Watch is not implemented
+func (s *healthServer) Watch(in *healthpb.HealthCheckRequest, srv healthpb.Health_WatchServer) error {
+	return status.Error(codes.Unimplemented, "Watch is not implemented")
+}
+
+func main() {
+	address := flag.String("address", ":50051", "address to listen on")
+	flag.Parse()
+	lis, err := net.Listen("tcp", *address)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &greeterServer{})
+	healthpb.RegisterHealthServer(s, &healthServer{})
+	log.Printf("Listening on address %s", *address)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
