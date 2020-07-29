e2234823e2cd97afe9506eb261ed9f822dbf9f85
swetasingh-tudip <sweta.singh@tudip.com>
2020-07-29 18:16:33 +0530

    - Updated "--enable-stackdriver-kubernetes" to enable Cloud Operations for GKE.

--- multicluster-gke/single-control-plane/scripts/1-cluster-create.sh
+++ multicluster-gke/single-control-plane/scripts/1-cluster-create.sh
@@ -26,7 +26,7 @@ log "Creating cluster1..."
 "https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring",\
 "https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly",\
 "https://www.googleapis.com/auth/trace.append" \
---num-nodes "2" --network "default" --enable-cloud-logging --enable-cloud-monitoring --enable-ip-alias --async
+--num-nodes "2" --network "default" --enable-stackdriver-kubernetes --enable-ip-alias --async
 
 sleep 20
 
@@ -37,4 +37,4 @@ log "Creating cluster2..."
 "https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring",\
 "https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly",\
 "https://www.googleapis.com/auth/trace.append" \
---num-nodes "2" --network "default" --enable-cloud-logging --enable-cloud-monitoring --enable-ip-alias --async
+--num-nodes "2" --network "default" --enable-stackdriver-kubernetes --enable-ip-alias --async
