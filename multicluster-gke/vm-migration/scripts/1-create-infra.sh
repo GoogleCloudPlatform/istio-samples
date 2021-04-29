581477b144dd4e31642ca38e226291f6aa0b4984
Sergey Kanzhelev <S.Kanzhelev@live.com>
2021-04-28 17:46:43 -0700

    Update 1-create-infra.sh

--- multicluster-gke/vm-migration/scripts/1-create-infra.sh
+++ multicluster-gke/vm-migration/scripts/1-create-infra.sh
@@ -21,6 +21,7 @@ log "🚀 Creating clusters..."
 gcloud config set project $PROJECT_ID
 
 gcloud container clusters create ${CLUSTER_1_NAME} --zone ${CLUSTER_1_ZONE} --username "admin" \
+--machine-type "n1-standard-4" \
 --scopes "https://www.googleapis.com/auth/compute","https://www.googleapis.com/auth/devstorage.read_only",\
 "https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring",\
 "https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly",\
@@ -28,6 +29,7 @@ gcloud container clusters create ${CLUSTER_1_NAME} --zone ${CLUSTER_1_ZONE} --us
 --num-nodes "4" --network "default" --enable-stackdriver-kubernetes  --async
 
 gcloud container clusters create ${CLUSTER_2_NAME} --zone ${CLUSTER_2_ZONE} --username "admin" \
+--machine-type "n1-standard-4" \
 --scopes "https://www.googleapis.com/auth/compute","https://www.googleapis.com/auth/devstorage.read_only",\
 "https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring",\
 "https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly",\
